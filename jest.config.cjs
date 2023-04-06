58f0ae07b1ab4b3e63e407a5e68f87dfa4b5847e
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-04-06 15:59:15 +0200

    Install dependencies for testing environment and setup configs

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  testMatch: ["**/src/**/*.test.ts", "**/src/**/*.test.tsx"],
+  resolver: "jest-ts-webcompat-resolver",
+  collectCoverageFrom: ["src/**/*.ts", "src/**/*.tsx"],
+};
