200c5b04e11dcd19c9709f07ed4ec0afa89d814d
victorandujar <victorandujarmurcia@gmail.com>
2023-04-08 23:49:21 +0200

    Add styles to Card component

---
+++ src/components/Card/CardStyled.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const CardStyled = styled.article`
+  color: black;
+
+  .card-title {
+    font-weight: 800;
+  }
+
+  .btn {
+    color: #fff;
+    cursor: pointer;
+  }
+`;
+
+export default CardStyled;
