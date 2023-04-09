ce685ac744b231aab78f485079250dd61e0610c6
victorandujar <victorandujarmurcia@gmail.com>
2023-04-09 18:40:10 +0200

    Change Card component styles

--- src/components/Card/CardStyled.tsx
+++ src/components/Card/CardStyled.tsx
@@ -2,14 +2,37 @@ import styled from "styled-components";
 
 const CardStyled = styled.article`
   color: black;
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+
+  .card {
+    &__info {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 20px;
+      font-weight: 600;
+      font-size: 15px;
+    }
+  }
+
+  .card-body {
+    flex: 0;
+    padding: 0;
+  }
 
   .card-title {
+    font-size: 14px;
     font-weight: 800;
+    padding: 0 20px;
   }
 
   .btn {
     color: #fff;
     cursor: pointer;
+    font-size: 12px;
+    width: 120px;
   }
 `;
 
