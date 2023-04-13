6030008ed78be8fe219b936de1389a74ab14dcc0
victorandujar <victorandujarmurcia@gmail.com>
2023-04-13 12:09:35 +0200

    Create getGame function at useApi custom hook

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
-import { GamesStructure } from "../types/types";
+import { GameStructure, GamesStructure } from "../types/types";
 import { useCallback, useContext } from "react";
 import GamesContext from "../store/contexts/GamesContext";
 import { loadGamesActionCreator } from "../store/actions/gamesActionsCreator";
+
+const urlApiSingleGame =
+  "https://free-to-play-games-database.p.rapidapi.com/api/game";
 
 const useApi = () => {
   const { dispatch } = useContext(GamesContext);
@@ -29,7 +32,34 @@ const useApi = () => {
     dispatch(loadGamesActionCreator(games));
   }, [dispatch]);
 
-  return { getGames };
+  const getGame = useCallback(
+    async (idGame: number) => {
+      const options = {
+        method: "GET",
+        url: `${urlApiSingleGame}?id=${idGame}`,
+        headers: {
+          "X-RapidAPI-Key":
+            "e13f113c85mshc4f7847a00d282ep13b377jsn09156613e306",
+          "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+        },
+      };
+
+      const game = (await axios
+        .request(options)
+        .then(function (response) {
+          return response.data;
+        })
+        .catch(function (error) {
+          return (error as Error).message;
+        })) as GameStructure;
+
+      const games = Array(game);
+
+      dispatch(loadGamesActionCreator(games));
+    },
+    [dispatch]
+  );
+  return { getGames, getGame };
 };
 
 export default useApi;
