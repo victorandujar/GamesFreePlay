11164c44b7a338b9d3035eb16812106063c97f01
victorandujar <victorandujarmurcia@gmail.com>
2023-04-17 16:11:50 +0200

    Add LoadGame action to the store

--- src/store/actions/gamesActionsCreator.ts
+++ src/store/actions/gamesActionsCreator.ts
@@ -1,9 +1,20 @@
-import { GamesStructure } from "../../types/types";
-import { GamesActionType, LoadGamesAction } from "../types/types";
+import { GameDetailStructure, GamesStructure } from "../../types/types";
+import {
+  GamesActionType,
+  LoadGameAction,
+  LoadGamesAction,
+} from "../types/types";
 
 export const loadGamesActionCreator = (
   games: GamesStructure
 ): LoadGamesAction => ({
   type: GamesActionType.loadGames,
   payload: games,
+});
+
+export const loadGameActionCreator = (
+  game: GameDetailStructure
+): LoadGameAction => ({
+  type: GamesActionType.loadGame,
+  payload: game,
 });
