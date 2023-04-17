bc87bd0c25e7d5aa4ffa1f2de027ee0642bea233
victorandujar <victorandujarmurcia@gmail.com>
2023-04-17 16:13:25 +0200

    Change structure from GamesContext and GamesContextProvider

--- src/store/contexts/GamesContextProvider.tsx
+++ src/store/contexts/GamesContextProvider.tsx
@@ -9,7 +9,30 @@ interface GamesContextProviderProps {
 const GamesContextProvider = ({
   children,
 }: GamesContextProviderProps): JSX.Element => {
-  const [games, dispatch] = useReducer(gamesReducer, []);
+  const [games, dispatch] = useReducer(gamesReducer, {
+    games: [],
+    game: {
+      description: "",
+      developer: "",
+      game_url: "",
+      genre: "",
+      id: 0,
+      minimum_system_requirements: {
+        graphics: "",
+        memory: "",
+        os: "",
+        processor: "",
+        storage: "",
+      },
+      platform: "",
+      publisher: "",
+      release_date: "",
+      screenshots: [{ image: "" }, { image: "" }, { image: "" }],
+      short_description: "",
+      thumbnail: "",
+      title: "",
+    },
+  });
   const gamesProps = useMemo(() => ({ games, dispatch }), [games]);
 
   return (
